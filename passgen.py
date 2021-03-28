00689b818979a52611d42d25907b7c0296724440
Marcin <dead0user@gmail.com>
2021-03-28 13:24:47 +0200

    Bad -f argument returns help

--- passgen.py
+++ passgen.py
@@ -1,53 +1,42 @@
-import random
 import argparse
+import sys
+import fgen
 
 small_letters = 'abcdefghijklmnopqrstuvwxyz'
 large_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 digits = '0123456789'
 special_signs = '!@#$%&?'
-# password_length = 15
-# passwords_quantity = 5
 
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument('-f', action='store',
+parser.add_argument('-f', action='store', required=True,
                     dest='force_of_pass', type=int,
                     help="Force of password(1:low, 2:mid, 3:high)")
-parser.add_argument('-q', action='store',
+parser.add_argument('-q', action='store', required=True,
                     dest='passwords_quantity', type=int,
                     help="Quantity of generated password")
-parser.add_argument('-l', action='store',
+parser.add_argument('-l', action='store', required=True,
                     dest='password_length', type=int,
-                    help="Length of generated passwords")
+                    help="Length of generated passwords(from 5 to 60 chars)")
+
+parser.parse_args(args=None if sys.argv[1:] else ['--help'])
 
 argument = parser.parse_args()
 
-def string_to_list(word):
-    return [char for char in word]
-
-def password_generator(pass_len, pass_dict):
-    password = ''
-    counter = 0
-    for sign in random.sample(pass_dict, pass_len):
-        password += sign
-        counter += 1
-        if counter == pass_len:
-            print(password)
-
-
 if argument.force_of_pass == 1:
-    pass_dictionary = string_to_list(digits + small_letters)
+    pass_dictionary = fgen.string_to_list(digits + small_letters)
 elif argument.force_of_pass == 2:
-    pass_dictionary = string_to_list(digits + small_letters + large_letters)
+    pass_dictionary = fgen.string_to_list(digits + small_letters + large_letters)
 elif argument.force_of_pass == 3:
-    pass_dictionary = string_to_list(digits + small_letters + large_letters + special_signs)
+    pass_dictionary = fgen.string_to_list(digits + small_letters + large_letters + special_signs)
 else:
-    print("Bad argument: must be 1, 2 or 3 !!!")
-    pass
+    parser.print_help()
+    sys.exit(0)
 
-
-# pass_dictionary = string_to_list(digits + special_signs + small_letters + large_letters)
+if argument.password_length < 5 and argument.password_length > 60:
+    parser.print_help()
+    sys.exit(0)
 
 for i in range(argument.passwords_quantity):
-    password_generator(argument.password_length, pass_dictionary)
+    fgen.password_generator(argument.password_length, pass_dictionary)
